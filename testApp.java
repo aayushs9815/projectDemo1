e413e0916f6a961a0ec90262183b437ff65f123f
Aayush Singhi <43982350+iamaayush98@users.noreply.github.com>
2020-10-19 16:48:08 +0530

    Create testApp.java

---
+++ testApp.java
@@ -0,0 +1,10 @@
+public class testApp {
+  static void myMethod(String fname) {
+    System.out.println(fname + " joined");
+  }
+
+  public static void main(String[] args) {
+    myMethod("Aayush");
+    myMethod("Yuvaan");
+    myMethod("Mudit");
+  }
